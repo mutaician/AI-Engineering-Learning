ed91d03146c83db0cfcc8ad09f3ab8c4e6de1ac0
Cian <cyprianmutai5900@gmail.com>
2025-04-05 17:07:17 +0300

    Implement text chunking functionality and add movies dataset

--- EmbeddingsAndVectorDatabases/text-chunking/src/main.js
+++ EmbeddingsAndVectorDatabases/text-chunking/src/main.js
@@ -1,25 +1,36 @@
 import './style.css'
 
-
+import { createClient } from "@supabase/supabase-js";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 
+// Supabase config
+const privateKey = import.meta.env.VITE_SUPABASE_API_KEY
+const url = import.meta.env.VITE_SUPABASE_URL
+const supabase = createClient(url, privateKey)
+
 // LangChain text splitter
-async function splitDocument() {
-  const response = await fetch('/src/podcasts.txt');
+async function splitDocument(document) {
+  const response = await fetch(document);
   const text = await response.text();
-  // console.log(text);
 
   const splitter = new RecursiveCharacterTextSplitter({
-    separator: " ",
     chunkSize: 150,
     chunkOverlap: 15
   })
 
-  const output = await splitter.createDocuments([text]) 
-  console.log(output)
+  const output = await splitter.splitText(text) 
+  return output
 }
-splitDocument()
 
-document.querySelector('#app').innerHTML = "Embedding and Vector Databases"
+async function main() {
+  const contentSplits = await splitDocument('/src/movies.txt')
+  console.log(contentSplits)
+
+}
+
+main()
+
+
+document.querySelector('#app').innerHTML = "Embedding, Vector Databases and Text Chunking"
 

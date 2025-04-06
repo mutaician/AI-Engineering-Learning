9735c6ff3f5c9a0b2c289874e3d2c8de0e3dc8b9
Cian <cyprianmutai5900@gmail.com>
2025-04-06 21:16:48 +0300

    add goal and plan to build my agent

--- agents/agentSetup/src/main.js
+++ agents/agentSetup/src/main.js
@@ -8,22 +8,32 @@ const openai = new OpenAI({
   dangerouslyAllowBrowser: true
 });
 
-const weather = await getCurrentWeather() 
-const location = await getLocation()
+/**
+ * Goal - build an agent that can answer any questions that might reguire knowledge about my current location and the curent weather at my location
+ */
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    model: "meta-llama/llama-4-maverick:free",
-    messages: [
-      {
-        "role": "user",
-        "content": `Give me a list of activity ideas based on my current location of ${location} and weather of ${weather}`
-      }
-    ],
+/**
+ * PLAN:
+ * 1. Design a well-written ReAct prompt
+ * 2. Build a loop for my agent to run in
+ * 3. Parse any actions that my agent to run in
+ * 4. End Condition - final Answer is given
+ */
+
+
+// async function main() {
+//   const completion = await openai.chat.completions.create({
+//     model: "meta-llama/llama-4-maverick:free",
+//     messages: [
+//       {
+//         "role": "user",
+//         "content": `Give me a list of activity ideas based on my current location of ${location} and weather of ${weather}`
+//       }
+//     ],
     
-  });
+//   });
 
-  console.log(completion.choices[0].message.content);
-}
+//   console.log(completion.choices[0].message.content);
+// }
 
-main();
+// main();

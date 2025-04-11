8ab7ede68259012cc43a72b754c799a4bc3cda13
Cian <cyprianmutai5900@gmail.com>
2025-04-11 23:25:19 +0300

    smolagent introduction with gemini

--- HuggingFaceAgentsCourse/agentIntro/main.py
+++ HuggingFaceAgentsCourse/agentIntro/main.py
@@ -1,52 +1,21 @@
 from dotenv import load_dotenv
 import os
-from openai import OpenAI
+from smolagents import CodeAgent, LiteLLMModel, DuckDuckGoSearchTool
 
 
 load_dotenv()
-api_key = os.getenv("OPENROUTER_API_KEY")
+api_key = os.getenv("GEMINI_API_KEY")
 
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=api_key
+model = LiteLLMModel(
+    model_id="gemini/gemini-2.0-flash",
+    api_key=api_key,
+    max_tokens=8192
 )
 
-def use_tool(tool_name, input_text):
-    if tool_name == "calculator":
-        try:
-            result = eval(input_text)
-            return str(result)
-        except Exception as e:
-            return f"Error in calculation: {str(e)}"
-    return "Tool not found or invalid input."
+agent = CodeAgent(
+    tools=[DuckDuckGoSearchTool()],
+    model=model)
 
-messages = [
-    {
-        "role": "system",
-        "content": "You are a helpful assistant.Keep your responses short and concise."
-    }
-]
 
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == "exit":
-        break
+agent.run("Search for the best music recommendations for a party at the Wayne's mansion.")
 
-    # simulate tool usage detection
-    # i.e calculate 2 + 3 * 4
-    if user_input.lower().startswith("calculate"):
-        expression = user_input[len("calculate "):]
-        tool_response = use_tool("calculator", expression)
-        print(f"Assistant (tool): {tool_response}")
-        messages.append({"role": "user", "content": user_input})
-        messages.append({"role": "assistant", "content": tool_response})
-    else:
-        messages.append({"role": "user", "content": user_input})
-
-        response = client.chat.completions.create(
-            model="openrouter/optimus-alpha",
-            messages=messages,
-        )
-        messages.append({"role": "assistant", "content": response.choices[0].message.content})
-
-        print(response.choices[0].message.content)

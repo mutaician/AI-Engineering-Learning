3b21c9c7337879aa86350ae8f53e3ccf91137564
Cian <cyprianmutai5900@gmail.com>
2025-08-11 12:48:36 +0300

    prompting workthrough

---
+++ ready-tensor-course/week2/README.md
@@ -0,0 +1,15 @@
+# Building prompts for Agentic AI Systems
+
+### Core components
+- Instruction -> What you want the AI to do
+- Input -> the content to work with
+### Optional but game changing components
+- Context -> Background information
+- Output format -> structure of the result
+- Role/Persona -> who the ai should act as
+- output constraints -> length, style
+- Tone/Style -> voice and approach to use
+- Examples
+- Goal -> underlying objective/purpose
+
+
---
+++ ready-tensor-course/week2/lesson1.py
@@ -0,0 +1,35 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from prompt_builder import build_prompt_from_config
+import yaml
+
+with open ("config/prompt_config.yaml") as f:
+    all_configs = yaml.safe_load(f)
+
+
+
+with open("data/publication.md", "r") as f:
+    publication_content = f.read()
+
+
+
+def main(prompt_config_key):
+    prompt_config = all_configs[prompt_config_key]
+    prompt = build_prompt_from_config(prompt_config, publication_content)
+    
+    with open(f"outputs/{prompt_cfg_key}_prompt.md", 'w') as f:
+        f.write(prompt)
+
+    llm = ChatOpenAI(model="gpt-5-nano", temperature=0)
+    message = HumanMessage(content=prompt)
+    response = llm.invoke([message])
+
+    print("="*40, "\nLLM OUTPUT\n", "="*40)
+    print(response.content)
+    
+    with open(f"outputs/{prompt_cfg_key}_llm_response.md", 'w') as f:
+        f.write(str(response.content))
+
+if __name__ == "__main__":
+    prompt_cfg_key = "summarization_prompt_cfg5"
+    main(prompt_cfg_key)
